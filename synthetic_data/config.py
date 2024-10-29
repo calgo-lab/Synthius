d5f49b4d01233e2f5013de7c5aef12c096021689
maryam-mohebbi <maryam.mohebbi.2@gmail.com>
2024-10-29 15:10:19 +0100

    initial commit

---
+++ synthetic_data/config.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+# Base directory for data
+data_base_path = Path.home() / "projects/synthetic-data/data"
+
+# Synthetic data variants
+SYNTHETIC_DATA_PATH = data_base_path / "synthetic"
+
+# Base directory for models
+MODELS_BASE_PATH = Path.home() / "projects/synthetic-data/models"
+
+# Result path
+RESULTS_PATH = Path.home() / "projects/synthetic-data/results"
