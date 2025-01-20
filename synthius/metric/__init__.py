681dd92fff11d7da5bda6bf08399febd6fd36b2d
maryam-mohebbi <maryam@mohebi.me>
2025-01-20 16:39:25 +0100

    Ruff

--- synthius/metric/__init__.py
+++ synthius/metric/__init__.py
@@ -1,7 +1,6 @@
 from .advanced_quality import AdvancedQualityMetrics
 from .basic_quality import BasicQualityMetrics
 from .distance import DistanceMetrics
-from .fairness import DistributionVisualizer, LogDisparityMetrics
 from .likelihood import LikelihoodMetrics
 from .linkability import LinkabilityMetric
 from .privacy_against_inference import PrivacyAgainstInference
@@ -12,10 +11,8 @@ __all__ = [
     "AdvancedQualityMetrics",
     "BasicQualityMetrics",
     "DistanceMetrics",
-    "DistributionVisualizer",
     "LikelihoodMetrics",
     "LinkabilityMetric",
-    "LogDisparityMetrics",
     "PrivacyAgainstInference",
     "PropensityScore",
     "SinglingOutMetric",
